306ef919c506790d28af6b25e67baefbf86bfe9f
saif <potelmyfreezy@gmail.com>
2025-11-22 14:41:56 +0200

    version 2

--- main.cpp
+++ main.cpp
@@ -3,16 +3,20 @@
 #include <map>
 using namespace std;
 
-vector<vector<char>> vec(6, vector<char>(7, ' ')) ;
-bool xo = true ;
-int turn ;
-int player = 1 ; 
-int winner = 0 ;
-map < int , int > mp = { {1, 0},{2,0},{3,0},{4,0},{5,0},{6,0},{7,0} };
+vector<vector<char>> grid(6, vector<char>(7, ' ')) ;
+bool playerSelector = true ;
+int playerMove ;
+char player = 'x' ; 
+bool winner = false ;
+map < int , int > storedMoves = { {1, 0},{2,0},{3,0},{4,0},{5,0},{6,0},{7,0} };
 void printgame(){
+    for(int j = 0 ; j < 7 ; j++){
+        cout << " " << (j+1) <<  " " ;
+    }
+    cout << "\n" ;
     for(int i = 0 ; i < 6 ; i++){
         for(int j = 0 ; j < 7 ; j++){
-            cout << '|' << vec[i][j] << '|' ;
+            cout << '|' << grid[i][j] << '|' ;
         }
         cout << "\n" ;
     }
@@ -22,9 +26,9 @@ void printgame(){
 void checker(){
     for(int i = 0 ; i < 6 ; i++){
         for(int j = 0 ; j < 7 ; j++){
-            if( (i <= 2 && vec[i][j] != ' ' && vec[i][j] == vec[i+1][j] && vec[i][j] == vec[i+2][j] && vec[i][j] == vec[i+3][j]) || (j <=3 && vec[i][j] != ' ' && vec[i][j] == vec[i][j+1] && vec[i][j] == vec[i][j+2] && vec[i][j] == vec[i][j+3]) || (i <= 2 && j <= 3 && vec[i][j] != ' ' && vec[i][j] == vec[i+1][j+1] && vec[i][j] == vec[i+2][j+2] && vec[i][j] == vec[i+3][j+3]) || (i <= 2 && j >= 3 && vec[i][j] != ' ' && vec[i][j] == vec[i+1][j-1] && vec[i][j] == vec[i+2][j-2] && vec[i][j] == vec[i+3][j-3])){
-                cout << "player " << vec[i][j] << " won\n" ;
-                winner++ ;
+            if( (i <= 2 && grid[i][j] != ' ' && grid[i][j] == grid[i+1][j] && grid[i][j] == grid[i+2][j] && grid[i][j] == grid[i+3][j]) || (j <=3 && grid[i][j] != ' ' && grid[i][j] == grid[i][j+1] && grid[i][j] == grid[i][j+2] && grid[i][j] == grid[i][j+3]) || (i <= 2 && j <= 3 && grid[i][j] != ' ' && grid[i][j] == grid[i+1][j+1] && grid[i][j] == grid[i+2][j+2] && grid[i][j] == grid[i+3][j+3]) || (i <= 2 && j >= 3 && grid[i][j] != ' ' && grid[i][j] == grid[i+1][j-1] && grid[i][j] == grid[i+2][j-2] && grid[i][j] == grid[i+3][j-3])){
+                cout << "player " << grid[i][j] << " won\n" ;
+                winner = true ;
                 return ;
             }
         }
@@ -32,26 +36,26 @@ void checker(){
 }
 
 void playerturn(int turn){
-    if (xo == true){
-        player = 1 ;
-        if(mp[turn] >= 6){
+    if (playerSelector == true){
+        player = 'x' ;
+        if(storedMoves[turn] >= 6){
             cout << "invalid move! \n" ;
             return ;
         }
-        vec[5 - mp[turn]][turn-1] = 'X';
-        mp[turn]++ ;
-        xo = false ;
-        player = 2 ; 
+        grid[5 - storedMoves[turn]][turn-1] = 'X';
+        storedMoves[turn]++ ;
+        playerSelector = false ;
+        player = 'o' ; 
     }
     else{
-        if(mp[turn] >= 6){
+        if(storedMoves[turn] >= 6){
             cout << "invalid move! \n" ;
             return ;
         }
-        vec[5 - mp[turn]][turn-1] = 'O';
-        mp[turn]++ ;
-        xo = true ;
-        player = 1 ;
+        grid[5 - storedMoves[turn]][turn-1] = 'O';
+        storedMoves[turn]++ ;
+        playerSelector = true ;
+        player = 'x' ;
     }
 }
 
@@ -60,19 +64,19 @@ int main(){
     cout << "Welcome to connect 4 game <3\n" ; 
     while(theLoop--){
         printgame() ; 
-        cout << "player " << player << " turn : " ;
+        cout << "player " << player << " turn , pick a column (1-7) : " ;
         while(true){
-            cin >> turn ;
-            if(turn < 1 || turn > 7 ){
+            cin >> playerMove ;
+            if(playerMove < 1 || playerMove > 7 ){
                 cout << "invalid move!\ntry again pls : " ;
             }
             else{
                 break;
             }
         }
-        playerturn(turn);
+        playerturn(playerMove);
         checker() ;
-        if(winner > 0){
+        if(winner){
             return 0 ;
         }
     }
